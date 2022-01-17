eb2ea1e02bd9029b1f525068f957ee5e02cdc1f6
egnoel <egnoel@hotmail.com>
2022-01-17 18:07:07 +0100

    end logger

---
+++ logger.js
@@ -0,0 +1,15 @@
+const eventEmitter = require('events');
+const path = require('path');
+const fs = require('fs');
+
+const emitter = new eventEmitter();
+
+emitter.on('log', (message)=>{
+    fs.appendFile(path.join(__dirname, 'log.txt'), message, (err)=>{if (err) throw err;})
+})
+
+function log(message){
+    emitter.emit('log',message);
+}
+
+module.exports = log
